7b836b94dc35813beff0a439b737319d17cdf7e3
Monica Thomas <monicathomas8@icloud.com>
2025-01-13 16:25:22 +0000

    added link to read a full story, html and updated views.py

--- stories/urls.py
+++ stories/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.story_list, name='story_list'),  # URL for the list page
+    path('<slug:slug>/', views.story_detail, name='story_detail'),
 ]
--- stories/views.py
+++ stories/views.py
@@ -6,3 +6,7 @@ from .models import Story
 def story_list(request):
     stories = Story.objects.filter(status=1)  # Fetch only published stories
     return render(request, 'story_list.html', {'stories': stories})
+
+def story_detail(request, slug):
+    story = get_object_or_404(Story, slug=slug)
+    return render(request, 'story_detail.html', {'story': story})
