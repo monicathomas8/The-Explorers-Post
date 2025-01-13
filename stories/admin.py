9bcd7ddaf17cc43a7b10512faca87755c548cc74
Monica Thomas <monicathomas8@icloud.com>
2025-01-13 17:01:42 +0000

    fixed summernote to the class storyadmin

--- stories/admin.py
+++ stories/admin.py
@@ -5,7 +5,10 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
 @admin.register(Story)
-class StoryAdmin(admin.ModelAdmin):
+class StoryAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'get_status_display', 'created_on')
     list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title', 'content')
+    summernote_fields = ('content')
+
