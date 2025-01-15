24f5b6e8e871f4c334f78de4eccc9bfba4fd5f20
Monica Thomas <monicathomas8@icloud.com>
2025-01-15 15:46:41 +0000

    imported slugify

--- stories/models.py
+++ stories/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.text import slugify
+from django.utils.timezone import now
+
 
 
 # Create your models here.
