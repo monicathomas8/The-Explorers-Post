a64af109af77f4a415ec9164d94238caae7fd8cc
Monica Thomas <monicathomas8@icloud.com>
2025-01-19 16:36:48 +0000

    tested contact form and add the contact model to the django admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('name', 'email', 'subject', 'message')
