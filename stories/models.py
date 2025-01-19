d24525fbd500b0883c733ea98dc39f233f224099
Monica Thomas <monicathomas8@icloud.com>
2025-01-19 19:09:48 +0000

    added social links in footer, styled it and make the login message all inline

--- stories/models.py
+++ stories/models.py
@@ -5,13 +5,18 @@ from django.utils.text import slugify
 from django.utils.timezone import now
 
 
-
-# Create your models here.
 STATUS = (
     (0, "Draft"),
     (1, "Published"),
 )
 
+
+"""
+Represents a story with a title, location, content, author, and status.
+
+Includes functionality for generating unique slugs, retrieving absolute URLs,
+and tracking creation and update timestamps.
+"""
 class Story(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True, blank=True)
@@ -23,6 +28,9 @@ class Story(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     
+    """
+    Custom save method to generate a unique slug based on the title if not provided.
+    """
     def save(self, *args, **kwargs):
         if not self.slug:
             base_slug = slugify(self.title)
@@ -35,8 +43,14 @@ class Story(models.Model):
         super().save(*args, **kwargs)
     
 
+    """
+    Returns the absolute URL for the story detail page.
+    """
     def get_absolute_url(self):
         return reverse('story_detail', kwargs={'slug': self.slug})
 
+    """
+    Returns the string representation of the story (its title).
+    """
     def __str__(self):
         return self.title
