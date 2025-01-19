e6d5569fbb1e91ff80b482a76fa7cb6cb7e5ad7c
Monica Thomas <monicathomas8@icloud.com>
2025-01-19 14:13:19 +0000

    fixed delete story path

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.my_posts, name='my_posts'),
-    path('<int:post_id>/', views.edit_story, name='edit_story'),
-    path('<int:post_id>/', views.delete_story, name='delete_story'),
+    path('<int:post_id>/edit', views.edit_story, name='edit_story'),
+    path('<int:post_id>/delete', views.delete_story, name='delete_story'),
 ]
